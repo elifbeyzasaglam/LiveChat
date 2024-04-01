bd08983430a0e8bec1664275bf6aef0732d329cd
Elif Beyza <saglame99@gmail.com>
2024-04-01 22:50:37 +0300

    Update AuthVC
    refactored and changed logic.

--- LiveChat/Modules/Auth/Controller/AuthVC.swift
+++ LiveChat/Modules/Auth/Controller/AuthVC.swift
@@ -6,57 +6,76 @@
 //
 
 import UIKit
-import Firebase
 import FirebaseAuth
 
 class AuthVC: UIViewController {
+    
+    enum AuthVCStatus {
+        case signIn
+        case signUp
+    }
     
     @IBOutlet weak var emailTextField: UITextField!
     
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var passwordConfirmTextField: UITextField!
     
+    @IBOutlet private weak var titleLabel: UILabel!
+    private var authVCStatus: AuthVCStatus = .signIn
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
-        
-        Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { result, error in
-            if let error {
-                print(error)
-            } else {
-                print("user id: \(result?.user.uid ?? "")")
+    }
+    
+    @IBAction func signUpButton(_ sender: UIButton) {
+        if authVCStatus == .signIn {
+            titleLabel.text = "Sign up to your account"
+            authVCStatus = .signUp
+            passwordConfirmTextField.isHidden = false
+        } else {
+            if let mail = emailTextField.text,
+               let password = passwordTextField.text {
+                AuthManager.shared.signUp(mail: mail, password: password) { [weak self] result in
+                    switch result {
+                    case .success(let user):
+                        print(user.uid)
+                        self?.navigateToMainVC()
+                    case .failure(let error):
+                        print(error.localizedDescription)
+                    }
+                }
             }
         }
     }
     
-    @IBAction func didTappedSignUpButton(_ sender: Any) {
-        //        passwordConfirmTextField.isHidden.toggle()
-        Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { result, error in
-            if let error {
-                print(error)
-            } else {
-                print("user id: \(result?.user.uid ?? "")")
+    @IBAction func signInButton(_ sender: UIButton) {
+        if authVCStatus == .signUp {
+            titleLabel.text = "Sign in to your account"
+            authVCStatus = .signIn
+            passwordConfirmTextField.isHidden = true
+        } else {
+            if let mail = emailTextField.text,
+               let password = passwordTextField.text {
+                AuthManager.shared.signIn(mail: mail, password: password) { [weak self] result in
+                    switch result {
+                    case .success(let user):
+                        print(user.uid)
+                        self?.navigateToMainVC()
+                    case .failure(let error):
+                        print(error)
+                    }
+                }
             }
         }
     }
     
-    @IBAction func signInButton(_ sender: Any) {
-        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) { [weak self] user, error in
-                   if let error = error {
-                       print(error.localizedDescription)
-                   } else {
-                       print("user id: \(user?.user.uid ?? "")")
-                       self?.navigateToMessageVC()
-                   }
-               }
+    private func navigateToMainVC() {
+        let mainVC = MainVC()
+        let navigationController = UINavigationController(rootViewController: mainVC)
+        navigationController.modalPresentationStyle = .fullScreen
+        present(navigationController, animated: true)
     }
-private func navigateToMessageVC() {
-    let messageVC = MessageVC(nibName: "MessageVC", bundle: nil)
-       navigationController?.pushViewController(messageVC, animated: true)
-        }
 
-    
     @IBAction func forwardPasswordButton(_ sender: Any) {
     }
     
