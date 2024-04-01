ab036a74dd0a64af3b19c1f953288f996ad5de18
Elif Beyza <saglame99@gmail.com>
2024-04-01 22:50:04 +0300

    mainVC
    created MainVC

---
+++ LiveChat/Modules/Main/MainVC.swift
@@ -0,0 +1,17 @@
+//
+//  MainVC.swift
+//  LiveChat
+//
+//  Created by ELİF BEYZA SAĞLAM on 31.03.2024.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
