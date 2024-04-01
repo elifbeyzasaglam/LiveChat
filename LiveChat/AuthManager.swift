64c1e3b25baf6d7262fc8fde0b203186e7b7f153
Elif Beyza <saglame99@gmail.com>
2024-04-01 22:50:50 +0300

    Create AuthManager.swift
    created AuthManager.

---
+++ LiveChat/AuthManager.swift
@@ -0,0 +1,33 @@
+//
+//  AuthManager.swift
+//  LiveChat
+//
+//  Created by ELİF BEYZA SAĞLAM on 1.04.2024.
+//
+
+import Foundation
+import FirebaseAuth
+
+final class AuthManager {
+    static let shared = AuthManager()
+    
+    func signUp(mail: String, password: String, completion: @escaping (Result<User, Error>) -> ()) {
+        Auth.auth().createUser(withEmail: mail, password: password) { result, error in
+            if let error {
+                completion(.failure(error))
+            } else if let user = result?.user {
+                completion(.success(user))
+            }
+        }
+    }
+    
+    func signIn(mail: String, password: String, completion: @escaping (Result<User, Error>) -> ()) {
+        Auth.auth().signIn(withEmail: mail, password: password) { result, error in
+            if let error = error {
+                completion(.failure(error))
+            } else if let user = result?.user {
+                completion(.success(user))
+            }
+        }
+    }
+}
