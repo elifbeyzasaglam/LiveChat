0b7069c09d1ced0bd343da4aa64a2d6d6d7cbbec
Elif Beyza <saglame99@gmail.com>
2024-04-14 12:10:47 +0300

    created userModel

---
+++ LiveChat/UserModel.swift
@@ -0,0 +1,15 @@
+//
+//  UserModel.swift
+//  LiveChat
+//
+//  Created by ELİF BEYZA SAĞLAM on 14.04.2024.
+//
+
+import Foundation
+
+struct UserModel: Codable {
+    let id: String
+    let name: String
+    let lastName: String
+    let mail: String
+}
