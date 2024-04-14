073548ab53e9f810ea08c7131d0a16102ccc47d5
Elif Beyza <saglame99@gmail.com>
2024-04-14 12:10:33 +0300

    created realtimeDatabaseManager.

---
+++ LiveChat/RealtimeDatabaseManager.swift
@@ -0,0 +1,24 @@
+//
+//  RealtimeDatabaseManager.swift
+//  LiveChat
+//
+//  Created by ELİF BEYZA SAĞLAM on 14.04.2024.
+//
+
+import Foundation
+import FirebaseDatabase
+
+final class RealtimeDatabaseManager {
+    static let shared = RealtimeDatabaseManager()
+    
+    private init() { }
+    
+    func saveUser(with model: UserModel, authId: String) {
+        let ref = Database.database().reference()
+        do {
+            try ref.child("users").child(authId).setValue(from: model)
+        } catch {
+            print(error)
+        }
+    }
+}
